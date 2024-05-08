4bb032616f1340991aaa576ede6d20b0e8d7a4a1
Abdelrahman Ragab <49238901+AbdelrahmanRagab38@users.noreply.github.com>
2024-05-08 20:01:17 +0300

    Update test_say_hi.py

--- tests/test_say_hi.py
+++ tests/test_say_hi.py
@@ -9,8 +9,6 @@ class TestSayHi(unittest.TestCase):
         # Test case 2: When passing an empty string, it should return "Hi"
         self.assertEqual(say_hi(""), "Hi ")
 
-        # Test case 3: When passing None, it should return "Hi None"
-        self.assertEqual(say_hi(None), "Hi None")
 
 if __name__ == '__main__':
     unittest.main()
